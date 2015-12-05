d66d9f8b748125539240418fef0fbbe84dfaf7f3
Calvinball Jones <equestrianartandscience@gmail.com>
2015-12-05 14:23:58 -0800

    Starts project with classes

---
+++ blog_classes.py
@@ -0,0 +1,18 @@
+class ResultsPage(object):
+    def __init__(self, url):
+        """
+        Takes a url and constructs a ResultsPage object.
+        """
+        # Gets the page HTML from the url.
+        # Gets the link for the next page (or sets the value to None).
+        # Separates the posts out into separate Post objects and saves them as a list.
+        # TODO: Add Post code.
+        pass
+
+
+class Post(object):
+    pass
+
+
+class Tag(object):
+    pass
---
+++ utils.py
@@ -0,0 +1,2 @@
+def timestamp_to_datetime():
+    pass
